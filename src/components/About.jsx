fe06cde796928f2251ab0b35d9666d432c90eb72
Ritesh S <riteshvshekhar03@gmail.com>
2022-10-22 13:32:19 +0530

    services changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,12 +10,12 @@ const About = () => {
                         <img src="/assets/about1.jpg" alt="About" className="w-75 mt-5"/>
                      </div>
                      <div className="col-md-6">
-                        <h3 className="fs-5 mb-0">About Us</h3>
-                        <h1 className="display-6 mb-2">Who <b>We</b> Are</h1>
+                        <h3 className="fs-5 mb-0"><br></br>About Us</h3>
+                        <h1 className="display-6 mb-2">Who <b>We</b> Are?</h1>
                         <hr className="w-50"/>
-                        <p className="lead mb-4">Our website takes the lead in online donor pledges and is now dominating the online space for pledges. Our form is easy to fill and submit on the website, and the donor cards are couriered to all parts of India. We take pride in sending donor cards within 4 weeks to everyone who has taken the brave decision to pledge their organs, no matter where in India they are </p>
-                        <button className="btn btn-primary rounded-pill px-4 py-2">Get Started</button>
-                        <button className="btn btn-outline-primary rounded-pill px-4 py-2 ms-2">Get Started</button>
+                        <p className="lead mb-4">Our website takes the lead in online donor pledges. Our form is easy to fill and submit on the website, and the donor cards are couriered to all parts of India. We take pride in sending donor cards within 4 weeks to everyone who has taken the brave decision to pledge their organs, no matter where in India they are </p>
+                        
+                        <button className="btn btn-outline-primary rounded-pill px-4 py-2">Learn more</button>
                      </div>
                   </div>
                </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,66 +7,59 @@ const Services = () => {
                 <div className="conatiner my-5 py-5">
                     <div className="row">
                         <div className="col-12">
-                            <h3 className="fs-5 text-center mb-0">Our Services</h3>
+                            
                             <h1 className="dispaly-6 text-center mb-4">Know <b>Organ</b> Donation</h1>
                             <hr className="w-25 mx-auto" />
                         </div>
                     </div>
                     <div className="row mt-5">
                         <div className="col-md-4">
-                            <div class="card p-3" >
+                            <div class="card p-4" >
                                 <div class="card-body text-center">
-                                    <i className="fa fa-cogs fa-4x mb-4 text-primary"></i>
-                                    <h5 class="card-title mb-3 fs-4 fw-bold">Kidney Donation</h5>
-                                    <p class="card-text lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <img src="/assets/kidney.png" alt="Services"/>
+                                 
+                                    <h5 class="card-title mb-3 fs-3 fw-bold">Kidney Donation</h5>
+                                    <p class="card-text lead">Hundreds of thousands of people suffer from renal failure each year and undergo dialysis or await a kidney transplant.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4">
                             <div class="card p-3" >
                                 <div class="card-body text-center">
-                                    <i className="fa fa-mobile fa-4x mb-4 text-primary"></i>
-                                    <h5 class="card-title mb-3 fs-4 fw-bold">Lungs Donation</h5>
-                                    <p class="card-text lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <img src="/assets/lungs.png" alt="Services" />
+                                   <h5 class="card-title mb-3 fs-4 fw-bold"><br></br>Lungs Donation</h5>
+                                    <p class="card-text lead">A lung transplant is a surgery to remove a person’s diseased lung and replace it with a healthy lung from a deceased donor.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4">
                             <div class="card p-3" >
                                 <div class="card-body text-center">
-                                    <i className="fa fa-users fa-4x mb-4 text-primary"></i>
+                                <img src="/assets/heart.png" alt="Services" />
                                     <h5 class="card-title mb-3 fs-4 fw-bold">Heart Donation</h5>
-                                    <p class="card-text lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text lead">Heart transplants are done soon after brain death of the donor is determined. Machines keep the heart and other organs functioning until the transplant teams arrive.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4">
                             <div class="card p-3" >
                                 <div class="card-body text-center">
-                                    <i className="fa fa-laptop fa-4x mb-4 text-primary"></i>
+                                <img src="/assets/eye.png" alt="Services" />
                                     <h5 class="card-title mb-3 fs-4 fw-bold">Eyes Donation</h5>
-                                    <p class="card-text lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text lead">Donated eyes can be used to restore vision in people who are suffering from corneal blindness. The front, clear and transparent tissue of the eye called as cornea can be used to restore vision in a corneal blind person</p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4">
                             <div class="card p-3" >
                                 <div class="card-body text-center">
-                                    <i className="fa fa-file-code-o fa-4x mb-4 text-primary"></i>
+                                <img src="/assets/liver.png" alt="Services" />
                                     <h5 class="card-title mb-3 fs-4 fw-bold">Liver Donation</h5>
-                                    <p class="card-text lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <p class="card-text lead">Liver transplantation is the universal treatment for end-stage chronic liver disease (ESLD) or cirrhosis, and is a life-saving operation for patients with acute liver failure</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-4">
-                            <div class="card p-3" >
-                                <div class="card-body text-center">
-                                    <i className="fa fa-star-half-o fa-4x mb-4 text-primary"></i>
-                                    <h5 class="card-title mb-3 fs-4 fw-bold">Save Others</h5>
-                                    <p class="card-text lead">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
             </section>
